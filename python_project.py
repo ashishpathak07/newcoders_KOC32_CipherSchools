466fe9ba7a5a35057d5b746140ddfefeb1f11b43
ashishpathak07 <117093376+ashishpathak07@users.noreply.github.com>
2022-12-02 10:20:46 +0530

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -1,5 +1,6 @@
 import math
-import random
+import random                       #math and random  imported
+
 def pos_neg(a,b):
     if c > 0:
         print("       -positive")
@@ -14,7 +15,7 @@ def odd_even(a,b):
     elif c%2!=0:
         print("       -odd")
     else:
-        False                       #odd even checked here
+        False                       #odd - even checked here
 
                                       
 def prime_composite(a,b):
@@ -30,10 +31,9 @@ def prime_composite(a,b):
     if(factors == 2):
         print("       -Prime")
     else:
-        print("       -Composite")          #prime composite checked
+        print("       -Composite")  #prime - composite checked
+
 def run(a,b,c):
-        #random integer picked
-    
     print("     The number chosen randomly is:",c)
     print("     This number is :-")
     pos_neg(a,b)
@@ -48,14 +48,22 @@ while True:
     a=int(input(" Enter number 1:"))
     b=int(input(" Enter number 2:"))
 
-    #two integers taken from user
+                                    #two integers taken from user
     print("-------------------------")
     
-    if a>b:
-        print("invalid range")
-        
-        c = random.randint (b,a)
-        run(a,b,c)
+    if a>b:                         #If use enters wrong range
+        print("invalid range given")
+        x=input("Swap the numbers?, yes or no :")
+        if x=="yes" or x=="Yes" or x=="":
+            
+            print("numbers will be swapped, to create valid range")
+            c = random.randint (b,a)    #random integer picked,by swapping the numbers
+            run(a,b,c)
+        else:
+            print("numbers not swapped! enter range again")
+            continue
+
+        y=input("Do you want to run program again? \n press y (for yes) or n (for no):")
         if y=="n" or y=="N":
             break                   #used while loop to run the the program again
         else:
@@ -63,7 +71,7 @@ while True:
         continue
         
     else:
-        c = random.randint(a,b)
+        c = random.randint(a,b)     #random integer picked
         run(a,b,c)
         print("-------------------------")
         y=input("Do you want to run program again? \n press y (for yes) or n (for no):")
